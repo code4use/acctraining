5591121e6912054e849945f3c040374dd2750992
Dmitri Mikhalyov <dimam1969@gmail.com>
2019-04-07 15:47:51 +0200

    Add  Adding , Subtraction modes and buttons

--- my.js
+++ my.js
@@ -1,21 +1,44 @@
 var res_ok=0, res_bad=0;
 var startDatetime, startmSecondsime, myTimerIns;
-var allOperands = {firstOperand: 1, secondOperand: 1, trueResult: 1, assumedResult:1, theOperation:0};
+var allOperands = {firstOperand: 1, secondOperand: 1, trueResult: 1, assumedResult:1, theOperation:0, theOperationChar:""};
 
 $(document).ready(function(){
-    //Add buttons listeners
-    var numberkeys=document.getElementsByClassName("number");
-    for(var i=0;i<numberkeys.length;i++) {
-        numberkeys[i].addEventListener('click',function(){
+    //Add number buttons listeners
+    var thekeys=document.getElementsByClassName("number");
+    for(var i=0;i<thekeys.length;i++) {
+        thekeys[i].addEventListener('click',function(){
             let currentres=document.getElementById("res");
-            console.log(currentres.value);
             currentres.value+=this.innerText;
         })
     }
+    //Add operation mode buttons listeners
+    var thekeys=document.getElementsByClassName("opermode");
+    for(var i=0;i<thekeys.length;i++) {
+        thekeys[i].addEventListener('click',function(){
+            allOperands.theOperationChar=this.innerText;
+            switch(allOperands.theOperationChar) {
+                case "×":
+                    allOperands.theOperation=0;
+                break;
+                case "÷":
+                    allOperands.theOperation=1;
+                break;
+                case "+":
+                    allOperands.theOperation=2;
+                break;
+                case "-":
+                    allOperands.theOperation=3;
+                break;
+            }
+            allReset();
+        })
+    }
+
     //Initial reset
     allReset();
     //Start the timer
     myTimerIns = setInterval(theTimer, 1000);
+
 });
 
 function chkForm() {
@@ -34,22 +57,50 @@ function chkForm() {
 }
 
 function newOperands(curOperands) {
-    let intTmp1 = Math.floor(Math.random()*10);
-    let intTmp2 = Math.floor(Math.random()*10);
-    if( intTmp1 < 2 ) intTmp1 = 9-intTmp1;
-    if( intTmp2 < 2 ) intTmp2 = 9-intTmp2;
+    let intTmp1, intTmp2;
     switch (curOperands.theOperation) {
-    case 1:
-        //division
-        curOperands.firstOperand = intTmp1*intTmp2;
-        curOperands.secondOperand = intTmp1;
-        curOperands.trueResult = intTmp2;
+        case 3:
+            //subtraction
+            intTmp1 = Math.floor(Math.random()*100);
+            intTmp2 = Math.floor(Math.random()*100);
+            if(intTmp1>intTmp2) {
+                curOperands.firstOperand = intTmp1;
+                curOperands.secondOperand = intTmp2;
+                curOperands.trueResult = intTmp1 - intTmp2;
+            }
+            else {
+                curOperands.firstOperand = intTmp2;
+                curOperands.secondOperand = intTmp1;
+                curOperands.trueResult = intTmp2-intTmp1;
+            }
         break;
-    case 0:
-        // multiplication
-        curOperands.firstOperand = intTmp1;
-        curOperands.secondOperand = intTmp2;
-        curOperands.trueResult = intTmp1 * intTmp2;
+        case 2:
+            //addition
+            intTmp1 = Math.floor(Math.random()*50);
+            intTmp2 = Math.floor(Math.random()*50);
+            curOperands.firstOperand = intTmp1;
+            curOperands.secondOperand = intTmp2;
+            curOperands.trueResult = intTmp1 + intTmp2;
+        break;
+        case 1:
+            //division
+            intTmp1 = Math.floor(Math.random()*10);
+            intTmp2 = Math.floor(Math.random()*10);
+            if( intTmp1 < 2 ) intTmp1 = 9-intTmp1;
+            if( intTmp2 < 2 ) intTmp2 = 9-intTmp2;
+            curOperands.firstOperand = intTmp1*intTmp2;
+            curOperands.secondOperand = intTmp1;
+            curOperands.trueResult = intTmp2;
+        break;
+        case 0:
+            // multiplication
+            intTmp1 = Math.floor(Math.random()*10);
+            intTmp2 = Math.floor(Math.random()*10);
+            if( intTmp1 < 2 ) intTmp1 = 9-intTmp1;
+            if( intTmp2 < 2 ) intTmp2 = 9-intTmp2;
+            curOperands.firstOperand = intTmp1;
+            curOperands.secondOperand = intTmp2;
+            curOperands.trueResult = intTmp1 * intTmp2;
         break;                        
     }
 }
@@ -57,14 +108,13 @@ function newOperands(curOperands) {
 function newRound() {
     if (allOperands.trueResult==allOperands.assumedResult) {
         newOperands(allOperands);
+        $("input[name=num1]").val(String(allOperands.firstOperand));
+        $("input[name=num2]").val(String(allOperands.secondOperand));
     }
     else {
         $("input[name=hint]").val(String(allOperands.trueResult));
     }
-    $("input[name=num1]").val(String(allOperands.firstOperand));
-    $("input[name=num2]").val(String(allOperands.secondOperand));
     $("input[name=res]").val("");
-    $("input[name=res]").focus();
 }
 
 function allReset() {
@@ -72,20 +122,30 @@ function allReset() {
     $("input[name=res_ok]").val(String(res_ok));
     $("input[name=res_bad]").val(String(res_bad));
     $("input[name=hint]").val("");
-    if(allOperands.theOperation == 0) {
-        $("#operchar").html('&#8729;');
-        $("#modebutton").html('Деление');
+    switch(allOperands.theOperation) {
+        case 3:
+            $("#operchar").html('-');
+        break;
+        case 2:
+            $("#operchar").html('+');
+        break;
+        case 1:
+            $("#operchar").html('&div;');
+            $("#modebutton").html('Умножение');
+        break;
+        case 0:
+            $("#operchar").html('&times;');
+            $("#modebutton").html('Деление');
+        break;
     }
-    else {
-        $("#operchar").html('&#8758;');
-        $("#modebutton").html('Умножение');
-    }
+
     allOperands.trueResult = 1;
     allOperands.assumedResult = allOperands.trueResult;
     startDatetime = new Date();
     startmSeconds = startDatetime.getTime();    
     $("input[name=timer]").val("0:0");
     newRound();
+    $("input[name=res]").focus();
     return false;
 }
 
@@ -96,13 +156,4 @@ function theTimer() {
     let minutes = Math.floor(seconds / 60); 
     seconds %= 60;
     $("input[name=timer]").val(String(minutes)+":"+String(seconds));
-}
-
-function switchMode() {
-    if(allOperands.theOperation == 0) 
-        allOperands.theOperation = 1;
-    else 
-        allOperands.theOperation = 0;
-    allReset();
-    return false;
 }
